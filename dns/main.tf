43bd7c59e09d5e24f1a70863884fa912b55724e0
TheJuliuscanute <juliuscanute@touchcapture.net>
2019-04-08 14:37:34 +1000

    default subdomain to redirect to

--- dns/main.tf
+++ dns/main.tf
@@ -12,3 +12,11 @@ resource "aws_route53_record" "root_domain" {
     evaluate_target_health = "${var.alias_health_check}"
   }
 }
+
+resource "aws_route53_record" "sub_domain" {
+  zone_id = "${data.aws_route53_zone.primary_domain.zone_id}"
+  name = "www.${var.domain_name}"
+  type = "CNAME"
+  ttl = "60"
+  records = ["${var.domain_name}"]
+}
