3dd888433f82ea2a46cbcc25b8d07a13af4facc5
TheJuliuscanute <juliuscanute@touchcapture.net>
2019-04-09 11:24:05 +1000

    remove defaults for project name

--- codebuild/vars.tf
+++ codebuild/vars.tf
@@ -1,19 +1,19 @@
 variable project_name {
   description = "Name of the project"
   type = "string"
-  default = "test-project"
 }
 
 variable project_description {
   description = "Describe what this project does briefly"
   type = "string"
-  default = "test_codebuild_project"
 }
 
 variable iam_role_arn {
   description = "IAM Role ARN"
+  type = "string"
 }
 
 variable build_repository_name {
   description = "Name of the repository"
+  type = "string"
 }
