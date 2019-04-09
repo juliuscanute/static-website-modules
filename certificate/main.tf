049a21fe1a7e45ca5fb2026286354fcbe01acacc
TheJuliuscanute <juliuscanute@touchcapture.net>
2019-04-10 08:37:06 +1000

    remove validation workflow

--- certificate/main.tf
+++ certificate/main.tf
@@ -4,27 +4,27 @@ resource "aws_acm_certificate" "aws_website_certificate" {
   validation_method = "DNS"
 }
 
-resource "aws_acm_certificate_validation" "dns_validation" {
-  certificate_arn = "${aws_acm_certificate.aws_website_certificate.arn}"
-  validation_record_fqdns = [
-    "${aws_route53_record.cert_validation.fqdn}",
-    "${aws_route53_record.cert_validation_alt1.fqdn}"]
-}
-
-resource "aws_route53_record" "cert_validation" {
-  name = "${aws_acm_certificate.aws_website_certificate.domain_validation_options.0.resource_record_name}"
-  type = "${aws_acm_certificate.aws_website_certificate.domain_validation_options.0.resource_record_type}"
-  zone_id = "${var.zone_id}"
-  records = [
-    "${aws_acm_certificate.aws_website_certificate.domain_validation_options.0.resource_record_value}"]
-  ttl = 60
-}
-
-resource "aws_route53_record" "cert_validation_alt1" {
-  name = "${aws_acm_certificate.aws_website_certificate.domain_validation_options.1.resource_record_name}"
-  type = "${aws_acm_certificate.aws_website_certificate.domain_validation_options.1.resource_record_type}"
-  zone_id = "${var.zone_id}"
-  records = [
-    "${aws_acm_certificate.aws_website_certificate.domain_validation_options.1.resource_record_value}"]
-  ttl = 60
-}
+//resource "aws_acm_certificate_validation" "dns_validation" {
+//  certificate_arn = "${aws_acm_certificate.aws_website_certificate.arn}"
+//  validation_record_fqdns = [
+//    "${aws_route53_record.cert_validation.fqdn}",
+//    "${aws_route53_record.cert_validation_alt1.fqdn}"]
+//}
+//
+//resource "aws_route53_record" "cert_validation" {
+//  name = "${aws_acm_certificate.aws_website_certificate.domain_validation_options.0.resource_record_name}"
+//  type = "${aws_acm_certificate.aws_website_certificate.domain_validation_options.0.resource_record_type}"
+//  zone_id = "${var.zone_id}"
+//  records = [
+//    "${aws_acm_certificate.aws_website_certificate.domain_validation_options.0.resource_record_value}"]
+//  ttl = 60
+//}
+//
+//resource "aws_route53_record" "cert_validation_alt1" {
+//  name = "${aws_acm_certificate.aws_website_certificate.domain_validation_options.1.resource_record_name}"
+//  type = "${aws_acm_certificate.aws_website_certificate.domain_validation_options.1.resource_record_type}"
+//  zone_id = "${var.zone_id}"
+//  records = [
+//    "${aws_acm_certificate.aws_website_certificate.domain_validation_options.1.resource_record_value}"]
+//  ttl = 60
+//}
