d0bea084c8e669dee96e3764978cfad44b9bf315
TheJuliuscanute <juliuscanute@touchcapture.net>
2019-04-10 15:41:53 +1000

    list of hosted zone id

--- dns/output.tf
+++ dns/output.tf
@@ -1,7 +1,3 @@
-output zone_id_non_existent {
-  value = "${aws_route53_zone.primary_domain_non_existent.zone_id}"
-}
-
-output zone_id_existent {
-  value = "${data.aws_route53_zone.primary_domain_existent.zone_id}"
+output zone_id {
+  value = ["${data.aws_route53_zone.primary_domain_existent.*.zone_id}","${aws_route53_zone.primary_domain_non_existent.*.zone_id}"]
 }
