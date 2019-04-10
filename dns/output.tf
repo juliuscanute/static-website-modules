3dc8f0cef7c45bfaab74c318d81ed2c6693a5858
TheJuliuscanute <juliuscanute@touchcapture.net>
2019-04-10 15:36:35 +1000

    zone id existent terraform

--- dns/output.tf
+++ dns/output.tf
@@ -1,3 +1,7 @@
-output zone_id {
-  value = "${aws_route53_zone.primary_domain.zone_id}"
+output zone_id_non_existent {
+  value = "${aws_route53_zone.primary_domain_non_existent.zone_id}"
+}
+
+output zone_id_existent {
+  value = "${data.aws_route53_zone.primary_domain_existent.zone_id}"
 }
