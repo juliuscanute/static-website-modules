7425b0f963aad115f34ada0765f1c34c1e1efec5
TheJuliuscanute <juliuscanute@touchcapture.net>
2019-04-10 15:27:48 +1000

    name conflict resolution

--- dns/main.tf
+++ dns/main.tf
@@ -1,16 +1,16 @@
-data "aws_route53_zone" "primary_domain" {
+data "aws_route53_zone" "primary_domain_existent" {
   count = "${var.existing}"
   name = "${var.domain_name}"
 }
 
-resource "aws_route53_zone" "primary_domain" {
+resource "aws_route53_zone" "primary_domain_non_existent" {
   count = "${1 - var.existing}"
   name = "${var.domain_name}"
 }
 
-resource "aws_route53_record" "root_domain" {
+resource "aws_route53_record" "root_domain_non_existent" {
   count = "${1 - var.existing}"
-  zone_id = "${aws_route53_zone.primary_domain.zone_id}"
+  zone_id = "${aws_route53_zone.primary_domain_non_existent.zone_id}"
   name = "${var.domain_name}"
   type = "A"
   alias {
@@ -22,16 +22,16 @@ resource "aws_route53_record" "root_domain" {
 
 resource "aws_route53_record" "sub_domain" {
   count = "${1 - var.existing}"
-  zone_id = "${aws_route53_zone.primary_domain.zone_id}"
+  zone_id = "${aws_route53_zone.primary_domain_non_existent.zone_id}"
   name = "www.${var.domain_name}"
   type = "CNAME"
   ttl = "60"
   records = ["${var.domain_name}"]
 }
 
-resource "aws_route53_record" "root_domain" {
+resource "aws_route53_record" "root_domain_existent" {
   count = "${var.existing}"
-  zone_id = "${data.aws_route53_zone.primary_domain.zone_id}"
+  zone_id = "${data.aws_route53_zone.primary_domain_existent.zone_id}"
   name = "${var.domain_name}"
   type = "A"
   alias {
@@ -41,9 +41,9 @@ resource "aws_route53_record" "root_domain" {
   }
 }
 
-resource "aws_route53_record" "sub_domain" {
+resource "aws_route53_record" "sub_domain_existent" {
   count = "${var.existing}"
-  zone_id = "${data.aws_route53_zone.primary_domain.zone_id}"
+  zone_id = "${data.aws_route53_zone.primary_domain_existent.zone_id}"
   name = "www.${var.domain_name}"
   type = "CNAME"
   ttl = "60"
